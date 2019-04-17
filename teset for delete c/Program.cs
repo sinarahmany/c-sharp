c398e9b0aec895147b6cb7ae773cdd49cfaef07d
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-04-17 09:31:04 -0700

    kick off by sina

---
+++ teset for delete c/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teset_for_delete_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello c#");
+            Console.ReadLine();
+
+        }
+    }
+}
